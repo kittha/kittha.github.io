dc3ff6833dacf457cc7f6d5820b46af4a1331494
Kittanut T <tkittanut@gmail.com>
2021-01-24 09:48:46 +0700

    add onclick event.

--- js/myfirstjs.js
+++ js/myfirstjs.js
@@ -1,3 +1,11 @@
 function message(msg) {
   document.getElementById('output').innerHTML = "display event " + msg;
 }
+
+function setDate() {
+  document.getElementById('date').innerHTML = Date();
+}
+
+function setConsole(msg) {
+  console.log(msg);
+}
